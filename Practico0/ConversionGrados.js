c4040de63b34a099103acbcffa10a702d5bddf46
MatiVarani <matias.varani@gmail.com>
2023-05-20 12:08:18 -0300

    “Agrego

---
+++ Practico0/ConversionGrados.js
@@ -0,0 +1,8 @@
+//Conversion de Grados Fahrenheit (F) a grados celsius (C)
+
+let fahrenheit = 75;
+let celsius = (fahrenheit - 32 )*(5/9);
+console.log(celsius);
+
+//RTA: La conversion de 50F es de 10C.ç
+//RTA: La conversion de 75F es de 23.89C.
