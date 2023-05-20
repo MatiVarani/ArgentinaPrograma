9e3ceace81b74623d08d9e26093c180fe2cc434e
MatiVarani <matias.varani@gmail.com>
2023-05-20 10:54:06 -0300

    Agrego carpeta del practico 0

---
+++ Practico0/Multiplicacion.js
@@ -0,0 +1,9 @@
+//Multiplicacion
+
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+let multiplicacion = num1 * num2 * num3;
+console.log(multiplicacion)
+
+//RTA: El valor que devuelve el programa es de 204.
---
+++ Practico0/Resta.js
@@ -0,0 +1,8 @@
+//Resta
+
+let minuendo = 10;
+let sustraendo = 5;
+let resta = minuendo - sustraendo;
+console.log(resta);
+
+//RTA: el valor que devuelve la resta es de 5.
