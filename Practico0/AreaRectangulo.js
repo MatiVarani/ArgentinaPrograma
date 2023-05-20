e9adbd96979bfa52fcb3b6a811ece42dc0929adf
MatiVarani <matias.varani@gmail.com>
2023-05-20 12:18:18 -0300

    Practico N0 Finalizado

---
+++ Practico0/AreaRectangulo.js
@@ -0,0 +1,9 @@
+//Area Rectangulo
+
+let base = 12.5;
+let altura = 5.3;
+let area = base * altura;
+console.log(area);
+
+//RTA: El area del rectangulo base = 5cm y altura = 3cm tiene un area de 15cm2.
+//RTA: El area del rectangulo base = 12.5cm y altura = 5.3cm tiene un area de 66.25cm2.
---
+++ Practico0/Promedio3.js
@@ -0,0 +1,23 @@
+//Promedio 3 numeros
+
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let promedio = (num1 + num2 + num3)/3;
+console.log("El promedio de los siguientes numeros (" +num1 +"," +num2 +"," +num3 +") es " +promedio);
+
+num1 = -2;
+num2 = -3;
+num3 = -8;
+promedio = (num1 + num2 + num3)/3;
+console.log("El promedio de los siguientes numeros (" +num1 +"," +num2 +"," +num3 +") es " +promedio);
+
+num1 = -1.2;
+num2 = 5.3;
+num3 = -3.3;
+promedio = (num1 + num2 + num3)/3;
+console.log("El promedio de los siguientes numeros (" +num1 +"," +num2 +"," +num3 +") es " +promedio);
+
+//RTA: El promedio de los siguientes numeros (5,7,9) es 7.
+//RTA: El promedio de los siguientes numeros (-2,-3,-8) es -4,33.
+//RTA: El promedio de los siguientes numeros (-1.2,5.3,-3.3) es 0,267.
