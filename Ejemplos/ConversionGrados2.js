c3e91625a72e59e777fa3495c3579b508b7c6f79
MatiVarani <matias.varani@gmail.com>
2023-05-27 11:41:13 -0300

    Se agregan 3 ejercicios extras a la carpeta Ejemplos

---
+++ Ejemplos/ConversionGrados2.js
@@ -0,0 +1,7 @@
+//Conversion de Grados Celcius (C) a grados Fahrenheit (F)
+
+let celsius = 10;
+let fahrenheit = (celsius/(5/9))+32;
+console.log(fahrenheit);
+
+//RTA: La conversion de 10C es de 50C.
