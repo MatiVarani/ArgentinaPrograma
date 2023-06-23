b194da3171e00ed1ad76f633de3afc28ce90e074
MatiVarani <matias.varani@gmail.com>
2023-06-23 18:16:03 -0300

    Ejercicios Practico 3

---
+++ Practico3/lanzamiento.js
@@ -0,0 +1,11 @@
+for (let i = 10; i > -1  ; i--){
+    if (i === 10){
+        console.log("Cuenta regresiva %i", i);
+    }
+    else if (i === 0){
+        console.log("Lanzamiento!")
+    }
+    else {
+        console.log ("%i", i)
+    }
+}
---
+++ Practico3/listaDeInvitados.js
@@ -0,0 +1,28 @@
+let personas = ["Jose", "Maria", "Juan", "Andres", "Lionel", "Sofia", "Leandro", "Emilia"];
+
+let admitidos = [];
+let rechazados = [];
+
+for (let i = 0; i < personas.length  ; i++){
+    if (personas[i] === "Jose" || personas[i] === "Sofia"){
+        rechazados.push(personas[i]);
+    }
+    else {
+        admitidos.push(personas[i]);
+    }
+}
+
+console.log("\nLa lista de invitados admitidos es:");
+admitidos.forEach(a => console.log(a));
+
+console.log("\nLa lista de invitados rechazados es:");
+rechazados.forEach(a => console.log(a));
+
+admitidos.sort();
+rechazados.sort();
+
+console.log("\nLa lista ordenada de invitados admitidos es:");
+admitidos.forEach(a => console.log(a));
+
+console.log("\nLa lista ordenada de invitados rechazados es:");
+rechazados.forEach(a => console.log(a));
